dfa0e9046a4de10ccf15f3a09f85bc4feceadd9d
Adam Elmadani <adamelmadani0129@gmail.com>
2024-02-13 08:50:39 +0000

    6.task

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This python module contains BasicAuth class
+"""
+from api.v1.auth.auth import Auth
+"""
+Auth class from auth module
+"""
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth class
+    """
+    pass
